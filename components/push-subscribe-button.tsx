36c604cb7f2539d67ea7fae9b0e878e4aa3a28cc
sfrechette77 <steve.frechette@gmail.com>
2026-05-18 22:46:35 -0500

    Cast applicationServerKey to BufferSource

--- components/push-subscribe-button.tsx
+++ components/push-subscribe-button.tsx
@@ -106,7 +106,7 @@ export function PushSubscribeButton() {
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as BufferSource,
       })
 
       // Send subscription to server
